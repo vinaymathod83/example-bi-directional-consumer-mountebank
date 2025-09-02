9d9bce5cae4d7edea94a118eb310b3fe492073af
vinaymathod83 <vinay.ma@smartbear.com>
2025-09-02 10:49:54 +0530

    Update product.js

--- src/product.js
+++ src/product.js
@@ -1,10 +1,10 @@
 export class Product {
-  constructor({id, name, type}) {
-    if (!id || !name || !type) {
+  constructor({id, name, price}) {
+    if (!id || !name || !price) {
       throw Error("id, name and type are required properties")
     }
     this.id = id
     this.name = name
-    this.type = type
+    this.price = price
   }
 }
