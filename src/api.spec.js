dccf0d51b7a6f9358df039f348ba08154b77a0ae
vinaymathod83 <vinay.ma@smartbear.com>
2025-09-02 10:48:03 +0530

    Update api.spec.js

--- src/api.spec.js
+++ src/api.spec.js
@@ -27,7 +27,7 @@ describe("API Contract Test", () => {
   const expectedProduct = {
     id: "10",
     type: "CREDIT_CARD",
-    name: "28 Degrees",
+    price: "42",
   };
 
   describe("retrieving products", () => {
